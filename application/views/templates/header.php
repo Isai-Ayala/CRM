b8c2eade2dd6d627bbafdcb493435dab95791070
Isai-Ayala <mr.popcorner@hotmail.com>
2018-05-04 17:14:17 -0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title> CRM </title>
+		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="/"> CRM</a>
+				</div>
+				<div id="navbar">
+					<ul class="nav navbar-nav">
+						<li><a href="/"></a></li>
+						<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
